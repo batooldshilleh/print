99a9ccb655da97986a8d4c055c22166edd4b6587
Batool D Shilleh <93814390+batooldshilleh@users.noreply.github.com>
2022-07-15 22:12:19 +0300

    Add files via upload

---
+++ print.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  print("Hello world");
+
+  //print number
+  print(1);
+  print(2);
+  print(3);
+  print(4);
+  print(5);
+
+  //short story
+  print("hello ,my name is coper");
+  print("i'm 60 years old");
+  print("i like my nsme coper");
+  print("but, i don't like being 40 years old");
+}
